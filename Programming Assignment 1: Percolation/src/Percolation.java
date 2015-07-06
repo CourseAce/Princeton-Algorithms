61f64390417812d01014bc9ea588712e736ecb7f
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-07-06 11:42:56 +0800

    fix style

--- Programming Assignment 1: Percolation/src/Percolation.java
+++ Programming Assignment 1: Percolation/src/Percolation.java
@@ -4,77 +4,79 @@
 
 
 public class Percolation {
-    WeightedQuickUnionUF uf;
-    boolean[][] mat;
-    int[][] dirs = new int[][] {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    int N;
-    int HEAD;
-    int TAIL;
+    private WeightedQuickUnionUF uf;
+    private boolean[][] mat;
+    private int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    private int N;
+    private int HEAD;
+    private int TAIL;
 
     /**
      * create N-by-N grid, with all sites blocked
+     *
      * @param N
      */
     public Percolation(int N) {
-        if(N<=0)
+        if (N <= 0)
             throw new IllegalArgumentException();
 
         this.N = N;
-        this.uf = new WeightedQuickUnionUF(N*N+2);  // the last two is for dummies
+        this.uf = new WeightedQuickUnionUF(N * N + 2);  // the last two is for dummies
         this.mat = new boolean[N][N];
-        this.HEAD = N*N;
-        this.TAIL = N*N+1;
+        this.HEAD = N * N;
+        this.TAIL = N * N + 1;
     }
 
     private int flat(int i, int j) {
-        return i*this.N + j;
+        return i * this.N + j;
     }
 
     /**
      * open site (row i, column j) if it is not open already
+     *
      * @param i [1, N]
      * @param j [1, N]
      */
     public void open(int i, int j) {
         try {
-            i = i-1;
-            j = j-1;
+            i = i - 1;
+            j = j - 1;
             this.mat[i][j] = true;
-            for(int[] dir: this.dirs) {
-                int n_i = i + dir[0];
-                int n_j = j + dir[1];
-                if(n_i >= 0 && n_i < this.N && n_j >=0 && n_j < this.N && this.mat[n_i][n_j]) {
-                    this.uf.union(this.flat(i, j), this.flat(n_i, n_j));
+            for (int[] dir : this.dirs) {
+                int i1 = i + dir[0];
+                int j1 = j + dir[1];
+                if (i1 >= 0 && i1 < this.N && j1 >= 0 && j1 < this.N && this.mat[i1][j1]) {
+                    this.uf.union(this.flat(i, j), this.flat(i1, j1));
                 }
             }
             if (i == 0) {
                 this.uf.union(this.flat(i, j), this.HEAD);
-            } else if (i == N-1) {
+            } else if (i == N - 1) {
                 this.uf.union(this.flat(i, j), this.TAIL);
             }
-        }
-        catch (IndexOutOfBoundsException e) {
+        } catch (IndexOutOfBoundsException e) {
             throw new IndexOutOfBoundsException();
         }
     }
 
     /**
      * is site (row i, column j) open?
+     *
      * @param i [1, N]
      * @param j [1, N]
      * @return
      */
     public boolean isOpen(int i, int j) {
         try {
-            return this.mat[i-1][j-1];
-        }
-        catch (IndexOutOfBoundsException e) {
+            return this.mat[i - 1][j - 1];
+        } catch (IndexOutOfBoundsException e) {
             throw new IndexOutOfBoundsException();
         }
     }
 
     /**
      * is site (row i, column j) full?
+     *
      * @param i
      * @param j
      * @return
@@ -85,6 +87,7 @@ public class Percolation {
 
     /**
      * does the system percolate?
+     *
      * @return
      */
     public boolean percolates() {
@@ -93,6 +96,7 @@ public class Percolation {
 
     /**
      * test client (optional)
+     *
      * @param args
      */
     public static void main(String[] args) {
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Java
 
 # Environment Setup
-[java-algs4](http://algs4.cs.princeton.edu/mac/)
+Read instructions on installation of java and its dependencies: [java-algs4](http://algs4.cs.princeton.edu/mac/)
 
 ## CLI
 To run programm in the command line interface (CLI):
@@ -26,9 +26,10 @@ javac -cp "$jars" -g -encoding UTF-8 "$@"
 ```
 
 ## IntelliJ
-1. Open the root director as the project root
+To run programm in IntelliJ:
+1. Open the root director as the project root in IntelliJ
 1. Set language level to 6
-1. Add `algs4.jar` and `stdlib.jar` to project's external libraries
-1. In any assignment folder, mark `src` as `Sources Root`
-1. Create target folder `tagert/classes`, `target/test-classes`, and add them to project target folders. 
-1. To compile and run, add `maven` framework support and ignore the generated `src` folder.  
+1. Add `~/algs4/algs4.jar` and `~/algs4/stdlib.jar` to project's external libraries
+1. In each assignment folder, mark `src` as `Sources Root`
+1. Create target folder `tagert/classes`, `target/test-classes`, and add them to project target folders 
+1. To compile and run, add `maven` framework support and ignore the generated `src` folder
